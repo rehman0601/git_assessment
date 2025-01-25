3efc6ae5616dc3b3122fffbd44e9894ada9a3a0c
rehman0601 <rehman65438@gmail.com>
2025-01-25 14:15:55 +0530

    guess the number game

---
+++ p1.py
@@ -0,0 +1,20 @@
+import random
+
+def guess_number():
+    number_to_guess = random.randint(1, 100)
+    guess = None
+
+    print("Guess the number between 1 and 100")
+
+    while guess != number_to_guess:
+        guess = int(input("Enter your guess: "))
+
+        if guess < number_to_guess:
+            print("Too low!")
+        elif guess > number_to_guess:
+            print("Too high!")
+        else:
+            print("Congratulations! You guessed the number.")
+
+if __name__ == "__main__":
+    guess_number()
